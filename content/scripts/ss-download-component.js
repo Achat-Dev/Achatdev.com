171f9b58f95ea11367368069c30eca7b614c825c
Achat <l.s.schulz@gmx.de>
2023-03-30 15:53:24 +0200

    Fixed file extension of some scuffed series panel

--- content/scripts/ss-download-component.js
+++ content/scripts/ss-download-component.js
@@ -5,15 +5,25 @@ class SSDownloadComponent extends HTMLElement {
 
     connectedCallback() {
         let text = this.innerText;
+        let ending = checkForAttribute(this, "ending", "png");
         this.innerHTML = `
         <div class="grid-element">
-            <img class="grid-image" src="/content/images/thescuffedseries/screenshots/screenshot_ss${text}.png" alt="The Scuffed Series ${text} - Screenshot">
+            <img class="grid-image" src="/content/images/thescuffedseries/screenshots/screenshot_ss${text}.${ending}" alt="The Scuffed Series ${text} - Screenshot">
             <div class="grid-widget">
                 <img class="grid-widget-icon" src="/content/images/thescuffedseries/icons/icon_ss${text}.png" alt="The Scuffed Series ${text} - Icon">
                 <a type="button" href="https://achat.itch.io/the-scuffed-series-${text}" target="_blank">Download on itch.io</a>
             </div>
         </div>
         `;
+    }
+}
+
+function checkForAttribute(element, name, defaultValue) {
+    if (element.hasAttribute(name)) {
+        return element.getAttribute(name);
+    }
+    else {
+        return defaultValue;
     }
 }
 
