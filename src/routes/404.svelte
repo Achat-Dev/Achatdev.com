00fcec5abeb39e7bbcd096cceaca58637065125f
Achat <l.s.schulz@gmx.de>
2024-05-05 11:50:40 +0200

    Added custom 404 page v3

--- src/routes/404.svelte
+++
@@ -1,3 +0,0 @@
-<h1>[404]</h1>
-<hr/>
-<h4>Something's wrong... I'll call the admin!</h4>
---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,5 @@
+<div class="slide-container">
+    <h1>[404]</h1>
+    <hr/>
+    <h4>Something's wrong... I'll call the admin!</h4>
+</div>
