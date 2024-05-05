94497516ef17374ab27cb18b3d14531505f12f45
Achat <l.s.schulz@gmx.de>
2024-05-05 12:45:25 +0200

    Changed title of 404 page v2

--- src/routes/fallback/+page.svelte
+++ src/routes/fallback/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        document.title = "Oh no"
+    })
+</script>
+
 <div class="slide-container background-gradient">
     <h1>[404]</h1>
     <h4>Something's wrong... I'll call the admin!</h4>
