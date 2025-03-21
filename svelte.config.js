e330d3d193d030d4ef75c7f789ab4ecafe092664
Achat <l.s.schulz@gmx.de>
2025-03-21 15:17:37 +0100

    Fixed 404 fallback path

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: 'fallback.html'
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
