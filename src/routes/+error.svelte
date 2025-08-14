8835f3b89eb11b73d6cfdf1c9d182c6dd6e0307c
Achat <l.s.schulz@gmx.de>
2025-08-14 12:17:37 +0200

    Try to fix custom 404 page sometimes not working

--- src/routes/+error.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import { page } from '$app/state';
-    import ErrorPage from '$lib/error_page.svelte';
-</script>
-
-<ErrorPage status={page.status} message={page.error?.message} />
